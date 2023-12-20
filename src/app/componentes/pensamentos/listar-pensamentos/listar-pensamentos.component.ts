99edcedba553f92f563880552d2d6ef0d49090d2
SaoriKabuchi <rkabuchi@gmail.com>
2023-12-20 16:00:23 -0300

    final do modulo 4

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamentos.component.css'],
 })
 export class ListarPensamentosComponent {
-  listaPensamentos = [];
+  listaPensamentos = [
+    {
+      conteudo: 'passo informações para o componente filho',
+      autoria: 'componente pai',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo: 'minha propriedade é decorada com @input',
+      autoria: 'componente filho',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo:
+        'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
+      autoria: 'sem autoria',
+      modelo: 'modelo1',
+    },
+  ];
 }
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pensamento.component.css'],
 })
 export class PensamentoComponent implements OnInit {
-  pensamento = {
+  @Input() pensamento = {
     conteudo: 'Angular',
     autoria: 'Saori',
     modelo: 'modelo3',
@@ -14,4 +14,11 @@ export class PensamentoComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
+    }
+    return 'pensamento-p';
+  }
 }
